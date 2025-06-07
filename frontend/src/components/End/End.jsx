3ada0bf7db9726c25b0647c7f5adead89ae49e11
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-06-07 02:29:21 +0200

    Adding new components

---
+++ frontend/src/components/End/End.jsx
@@ -0,0 +1,12 @@
+import './End.css'
+
+export function End () {
+    return (
+        <footer className='end-footer'>
+            <div className='end-div'>
+                <p>© 2025 Juan Manuel</p>
+                <p>Contacto</p>
+            </div>
+        </footer>
+    )
+}
