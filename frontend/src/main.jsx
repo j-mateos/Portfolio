c55df102be869b0e175543e42c4e72c643c0c828
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-06-07 02:28:39 +0200

    Adding new components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,13 @@ import './main.css'
 
 // Components
 import { Menu } from './components/Menu/Menu'
+import { Content } from './components/Content/Content'
+import { End } from './components/End/End'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Menu />
-        <div className='prueba'>Hola</div>
+        <Content />
+        <End />
     </StrictMode>
 )
