b2a2c5baef8ed1c937f123550ad79aa557c7a4b2
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-06-08 15:35:52 +0200

    Adding a Svg component

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,14 +1,20 @@
 import './Menu.css'
 
+import { Svg } from '../Svg/Svg'
+
 export function Menu () {
     return (
         <header className='menu-header'>
             <nav className='menu-nav'>
                 <a>Presentación</a>
                 <a>Experiencia</a>
-                <a>Proyectos</a>
+                <a>Tecnologías</a>
                 <a>Sobre mí</a>
-                <a>Modo</a>
+                <a>
+                    <Svg name="lightMode" size={22} color="#ffffff" />
+                    <Svg name="darkMode" size={22} color="#ffffff" />
+                    <Svg name="lightMode" size={22} color="#ffffff" />
+                </a>
                 <a>Idioma</a>
             </nav>
         </header>
