b339fcd83b892a09f4f69c504e295adb1ca7e50d
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-06-15 22:04:03 +0200

    Adding href to the Button component

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -4,9 +4,9 @@ import './Button.css'
 import { Svg } from '../Svg/Svg'
 /* eslint-enable no-unused-vars */
 
-export function Button ({ name, text, size = 15, color = '#ffffff', viewBox = '0 -960 960 960' }) {
+export function Button ({ name, text, url, size = 15, color = '#ffffff', viewBox = '0 -960 960 960' }) {
     return (
-        <a className='button-a'>
+        <a href={url} className='button-a'>
             <Svg name={name} size={size} color={color} viewBox={viewBox} />
             <span>{text}</span>
         </a>
