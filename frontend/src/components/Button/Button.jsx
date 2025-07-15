d86495009d1e56ee924497aec2c10646014d7bac
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-07-15 18:41:26 +0200

    Updating old components

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -4,10 +4,10 @@ import './Button.css'
 import { Svg } from '../Svg/Svg'
 /* eslint-enable no-unused-vars */
 
-export function Button ({ name, text, url, size = 15, color = '#ffffff', viewBox = '0 -960 960 960' }) {
+export function Button ({ text, url, svgName, svgSize = 15, svgColor = '#ffffff' }) {
     return (
-        <a href={url} className='button-a'>
-            <Svg name={name} size={size} color={color} viewBox={viewBox} />
+        <a href={url} className='button'>
+            <Svg name={svgName} size={svgSize} color={svgColor} />
             <span>{text}</span>
         </a>
     )
