d24b47850ba7390b3ec742b4eeb27e174d062690
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-07-15 18:43:01 +0200

    Renaming old components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,15 @@ import { createRoot } from 'react-dom/client'
 import './variables.css'
 import './main.css'
 
-import { Menu } from './components/Menu/Menu'
+import { Header } from './components/Header/Header'
 import { Content } from './components/Content/Content'
-import { End } from './components/End/End'
+import { Footer } from './components/Footer/Footer'
 /* eslint-enable no-unused-vars */
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Menu />
+        <Header />
         <Content />
-        <End />
+        <Footer />
     </StrictMode>
 )
