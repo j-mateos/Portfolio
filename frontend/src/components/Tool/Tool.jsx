ae2905fd1fa5a28c23d61a2921ac41fb34311b0d
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-07-27 19:52:07 +0200

    Modifying SVG component

--- frontend/src/components/Tool/Tool.jsx
+++ frontend/src/components/Tool/Tool.jsx
@@ -1,14 +1,10 @@
 import './Tool.css'
 
-/* eslint-disable no-unused-vars */
-import { Svg } from '../Svg/Svg'
-/* eslint-enable no-unused-vars */
-
-export function Tool ({ text, svgName, svgSize = 30, svgColor = '#ffffff' }) {
+export function Tool ({ text, svgName, svgSize = 45 }) {
     return (
         <div className='tool'>
-            <Svg name={svgName} size={svgSize} color={svgColor} />
-            <p style={{ color: svgColor }}>{text}</p>
+            <img src={'/assets/svg/' + svgName + '.svg'} width={svgSize} height={svgSize} />
+            <p>{text}</p>
         </div>
     )
 }
