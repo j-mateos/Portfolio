bf6e68cfcf57ce1037b519a659cff5cdd11e259f
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2025-12-09 22:29:34 +0100

    Adding translation and dark mode

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,21 +1,47 @@
 import './Header.css'
 import { useState, useEffect, useRef } from 'react'
+import { useTheme } from '../App/ThemeContext.jsx'
+import { useTranslation } from 'react-i18next'
 
 /* eslint-disable no-unused-vars */
 import { Svg } from '../../ui/Svg/Svg'
-import { useTheme } from '../App/ThemeContext.jsx'
 /* eslint-enable no-unused-vars */
 
+const flagSvgs = import.meta.glob('../../../assets/svg/flag_*.svg', {
+    eager: true,
+    import: 'default'
+})
+
+// Obtains the flag URL based on the language key
+const getFlagSrc = (languageKey) => {
+    const fileName = `flag_${languageKey}.svg`
+    const key = Object.keys(flagSvgs).find(k => k.endsWith(fileName))
+    return flagSvgs[key]
+}
+
 export function Header () {
+    const { t, i18n } = useTranslation()
+
+    // Variables and state for theme modes
     const [, setMode, selectedMode] = useTheme()
     const [showModes, setShowModes] = useState(false)
     const modesRef = useRef(null)
     const modes = [
-        { key: 'system', label: 'Sistema' },
-        { key: 'light', label: 'Claro' },
-        { key: 'dark', label: 'Oscuro' }
+        { key: 'system', label: t('header.modeSystem') },
+        { key: 'light', label: t('header.modeLight') },
+        { key: 'dark', label: t('header.modeDark') }
     ]
 
+    // Variables and state for language selection
+    const currentLanguage = i18n.language
+    const [showLanguages, setShowLanguages] = useState(false)
+    const languagesRef = useRef(null)
+    const languages = [
+        { key: 'es', label: 'Español' },
+        { key: 'en', label: 'English' }
+    ]
+
+    // Methods to handle mode selection
     const handleCurrentModeClick = () => setShowModes(!showModes)
 
     const handleModeSelect = (selected) => {
@@ -24,9 +50,23 @@ export function Header () {
         setShowModes(false)
     }
 
+    // Methods to handle language selection
+    const handleCurrentLanguageClick = () => setShowLanguages(!showLanguages)
+
+    const handleLanguageSelect = (lang) => {
+        localStorage.setItem('app-language', lang)
+        i18n.changeLanguage(lang)
+        setShowLanguages(false)
+    }
+
+    // Close modes dropdown on outside click
     const handleClickOutside = (event) => {
         if (modesRef.current && !modesRef.current.contains(event.target)) {
             setShowModes(false)
+        }
+
+        if (languagesRef.current && !languagesRef.current.contains(event.target)) {
+            setShowLanguages(false)
         }
     }
 
@@ -40,19 +80,41 @@ export function Header () {
     return (
         <header className='header'>
             <nav className='header-nav'>
-                <a href='#presentation' className='nav-link'>Presentación</a>
-                <a href='#experience' className='nav-link'>Experiencia</a>
-                <a href='#technologies' className='nav-link'>Tecnologías</a>
-                <a href='#about-me' className='nav-link'>Sobre mí</a>
+                <a href='#presentation' className='nav-link'>{t('header.navPresentation')}</a>
+                <a href='#experience' className='nav-link'>{t('header.navExperience')}</a>
+                <a href='#technologies' className='nav-link'>{t('header.navTechnologies')}</a>
+                <a href='#about-me' className='nav-link'>{t('header.navAboutMe')}</a>
+
+                {/* Mode selector */}
                 <div ref={modesRef}>
-                    <div className='header-current-modes' onClick={handleCurrentModeClick}>
+                    <div className='header-current-state' onClick={handleCurrentModeClick}>
                         <Svg name={selectedMode + 'Mode'} />
                     </div>
                     {showModes && (
-                        <div className='header-modes'>
+                        <div className='header-states'>
                             {modes.map((m) => (
                                 <span key={m.key} onClick={() => handleModeSelect(m.key)}>
                                     {m.label}
+                                </span>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
+                {/* Language selector */}
+                <div ref={languagesRef}>
+                    <div className='header-current-state' onClick={handleCurrentLanguageClick}>
+                        <img
+                            src={getFlagSrc(currentLanguage)}
+                            alt={`Bandera de ${currentLanguage}`}
+                            width={25} height={25}
+                        />
+                    </div>
+                    {showLanguages && (
+                        <div className='header-states'>
+                            {languages.map((l) => (
+                                <span key={l.key} onClick={() => handleLanguageSelect(l.key)}>
+                                    {l.label}
                                 </span>
                             ))}
                         </div>
